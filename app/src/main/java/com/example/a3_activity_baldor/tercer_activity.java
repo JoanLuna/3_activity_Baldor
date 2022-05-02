78a14218f18eb8d19e60427fc779627c4fcdd0b0
Joan Luna Sosa <55028789+JoanLuna@users.noreply.github.com>
2022-05-02 16:10:57 -0600

    Solo Falta la primera

--- app/src/main/java/com/example/a3_activity_baldor/tercer_activity.java
+++ app/src/main/java/com/example/a3_activity_baldor/tercer_activity.java
@@ -3,13 +3,15 @@ package com.example.a3_activity_baldor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class tercer_activity extends AppCompatActivity {
 
-    Button regresar;
+    Button regresar,pagina_web,enviar_correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,33 @@ public class tercer_activity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        enviar_correo = (Button)findViewById(R.id.boton_correo);
+        enviar_correo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent envcorreo = new Intent(Intent.ACTION_SEND);
+                envcorreo.setType("message/rfc822");
+                envcorreo.putExtra(Intent.EXTRA_EMAIL, new String[]{"mexico@oceana.org"});
+                envcorreo.putExtra(Intent.EXTRA_SUBJECT, "Más Información");
+                envcorreo.putExtra(Intent.EXTRA_TEXT, "Me gustaría recibir más información sobre proteger a los océanos");
+                try {
+                    startActivity(Intent.createChooser(envcorreo,"Enviar Email"));
+                }catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getApplicationContext(),"No hay aplicación de correo instalada",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        pagina_web = (Button)findViewById(R.id.boton_web);
+        pagina_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrirWeb = new Intent(Intent.ACTION_VIEW);
+                abrirWeb.setData(Uri.parse("https://mx.oceana.org"));
+                startActivity(abrirWeb);
+            }
+        });
+
     }
 }
